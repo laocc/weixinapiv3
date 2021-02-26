3749c8ab4d0bc84ec6e7ae3e7d79209517970963
whdabai <1733886703@qq.com>
2021-02-27 05:06:22 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- mall/Bill.php
+++ mall/Bill.php
@@ -8,26 +8,37 @@ use esp\weiPay\ApiV3Base;
 
 class Bill extends ApiV3Base
 {
-    public function create(array $param)
+    public function create(array $billOrder)
     {
         $time = time();
-        $data = [];
-        $data['appid'] = $this->service->miniAppID;
-        $data['sub_mchid'] = $param['mchID'];
-        $data['transaction_id'] = $param['subTransactionID'];
-        $data['out_order_no'] = $param['billNumber'];
-        $data['finish'] = $param['finish'];
 
-        $data['receivers'] = [];
-
-        $data['receivers']['type'] = 'MERCHANT_ID';
-        $data['receivers']['receiver_account'] = $param['mchID'];
-        $data['receivers']['amount'] = 3333;
-        $data['receivers']['description'] = 3333;
+        $retValue = [];
+        $retValue[0] = ['finish' => 0, 'mchID' => $mchOrder['shopWxMchID'], 'receiver' => []];
+        $retValue[1] = ['finish' => 0, 'mchID' => $platOrder['shopWxMchID'], 'receiver' => []];
 
 
-        $unified = $this->post("/v3/ecommerce/profitsharing/orders", $data);
-        if (is_string($unified)) return $unified;
+        foreach ($billOrder as $i => $bill) {
+
+            $data = [];
+            $data['appid'] = $this->service->miniAppID;
+            $data['sub_mchid'] = $bill['mchID'];
+            $data['transaction_id'] = $param['subTransactionID'];
+            $data['out_order_no'] = $param['billNumber'];
+            $data['finish'] = $param['finish'];
+
+            $data['receivers'] = [];
+
+            $data['receivers']['type'] = 'MERCHANT_ID';
+            $data['receivers']['receiver_account'] = $param['mchID'];
+            $data['receivers']['amount'] = 3333;
+            $data['receivers']['description'] = 3333;
+
+
+            $unified = $this->post("/v3/ecommerce/profitsharing/orders", $data);
+            if (is_string($unified)) return $unified;
+
+
+        }
 
 
         return $values;
