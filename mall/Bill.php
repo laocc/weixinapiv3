f34f4ee23fcb3fb3c232a11b9e596f88c626751d
ddd <ddd@qqq.com>
2021-02-27 15:36:37 +0800

    Signed-off-by: ddd <ddd@qqq.com>

--- mall/Bill.php
+++ mall/Bill.php
@@ -3,45 +3,32 @@ declare(strict_types=1);
 
 namespace esp\weiPay\mall;
 
-
 use esp\weiPay\ApiV3Base;
 
 class Bill extends ApiV3Base
 {
+
+    /**
+     * @param array $billOrder
+     * @return array
+     *
+     * 分账回退通知的url，在服务商后台【交易中心】【分账】【分账接收设置】中配置
+     */
     public function create(array $billOrder)
     {
-        $time = time();
-
-        $retValue = [];
-        $retValue[0] = ['finish' => 0, 'mchID' => $mchOrder['shopWxMchID'], 'receiver' => []];
-        $retValue[1] = ['finish' => 0, 'mchID' => $platOrder['shopWxMchID'], 'receiver' => []];
-
-
+        $value = [];
         foreach ($billOrder as $i => $bill) {
-
             $data = [];
             $data['appid'] = $this->service->miniAppID;
             $data['sub_mchid'] = $bill['mchID'];
-            $data['transaction_id'] = $param['subTransactionID'];
-            $data['out_order_no'] = $param['billNumber'];
-            $data['finish'] = $param['finish'];
-
-            $data['receivers'] = [];
-
-            $data['receivers']['type'] = 'MERCHANT_ID';
-            $data['receivers']['receiver_account'] = $param['mchID'];
-            $data['receivers']['amount'] = 3333;
-            $data['receivers']['description'] = 3333;
-
-
+            $data['transaction_id'] = $bill['transaction'];
+            $data['out_order_no'] = $bill['number'];
+            $data['finish'] = $bill['finish'];
+            $data['receivers'] = $bill['receivers'];
             $unified = $this->post("/v3/ecommerce/profitsharing/orders", $data);
-            if (is_string($unified)) return $unified;
-
-
+            $value[$bill['number']] = $unified;
         }
-
-
-        return $values;
+        return $value;
     }
 
 }
