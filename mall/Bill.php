9d33ff83dca3553584187ba076bce618f7ae662d
ddd <ddd@qqq.com>
2021-02-27 17:15:21 +0800

    Signed-off-by: ddd <ddd@qqq.com>

--- mall/Bill.php
+++ mall/Bill.php
@@ -8,6 +8,19 @@ use esp\weiPay\ApiV3Base;
 class Bill extends ApiV3Base
 {
 
+    public function bind($param)
+    {
+        $data = [];
+        $data['appid'] = $this->service->miniAppID;
+        $data['type'] = 'PERSONAL_OPENID';
+        $data['account'] = $param['openid'];
+        $data['relation_type'] = 'DISTRIBUTOR';
+        $unified = $this->post("/v3/ecommerce/profitsharing/receivers/add", $data);
+        if (is_string($unified)) return $unified;
+        return true;
+    }
+
+
     /**
      * @param array $billOrder
      * @return array
@@ -16,6 +29,14 @@ class Bill extends ApiV3Base
      */
     public function create(array $billOrder)
     {
+        /**
+         * (
+         * [order_id] => 30002102162021022708255143656
+         * [out_order_no] => 2102271613399447597400
+         * [sub_mchid] => 1606823507
+         * [transaction_id] => 4313500652202102274472653205
+         * )
+         */
         $value = [];
         foreach ($billOrder as $i => $bill) {
             $data = [];
