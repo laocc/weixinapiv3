03fae1b3d9c23c422e7118696ab163400c0a7c88
whdabai <1733886703@qq.com>
2021-03-16 02:39:46 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- library/Entity.php
+++ library/Entity.php
@@ -48,6 +48,10 @@ class Entity
          * 这里用到的密钥是在微信支付后台申请的
          */
         $cert = _CERT . "/{$this->certSerial}/apiclient_key.pem";
+        if (!is_readable($cert)) {
+            $cert = _CERT . "/{$this->mchID}/apiclient_key.pem";
+            if (!is_readable($cert)) throw new EspError("商户证书文件不存在，请检查");
+        }
         $this->certEncrypt = \openssl_get_privatekey(\file_get_contents($cert));
         return $this;
     }
